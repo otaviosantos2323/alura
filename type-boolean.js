4abe0b10f1fb79a7ac00fc63acfaf191d595fc86
otaviosantos2323 <otavio.p.s.santos@gmail.com>
2022-06-18 17:41:33 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+// boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,24 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Otávio';
+
+// concatenação 
+
+console.log(citacao + meuNome);
+
+// template string, ou template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
