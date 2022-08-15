d4f63e7e0c0b2361ca93e52643badb7731a8da09
otaviosantos2323 <otavio.p.s.santos@gmail.com>
2022-08-15 14:00:13 -0300

    var e conversões

---
+++ script.js
@@ -0,0 +1,36 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+// let base = altura;
+
+// if (forma === 'retângulo') { 
+//     area = altura * comprimento;
+// } else { 
+//     area = (altura * base)/2;
+// }   
+
+// console.log(area)
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+const base = comprimento;
+let area;
+
+if (forma === 'retângulo') { 
+    area = altura * comprimento;
+} else { 
+    area = (altura * base)/2;
+}
+
+console.log(area)
